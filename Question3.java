ab06dcbae17e5d375d6a6f60707f6e6eb6ebfc76
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-16 02:34:12 +0530

    Add files via upload
    even numbers

---
+++ Question3.java
@@ -0,0 +1,26 @@
+
+// 3.write a program  to print all even number from 50 to 75.
+
+
+
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Even Number Given below from 50 to 75");
+		
+		
+		for (int i=50 ; i<=75; i++)
+		{
+			if (i %2 ==0 )
+			{
+				
+				System.out.println("--> "+ i);
+				
+			}
+		}
+
+	}
+
+}
